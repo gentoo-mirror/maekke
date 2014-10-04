77006a2b4eb2d57286756af22a3fc7b6ab01264a
Markus Meier <maekke@gentoo.org>
2014-10-04 12:37:05 +0200

    ignore cvs header

--- scripts/diff-gentoo-x86.sh
+++ scripts/diff-gentoo-x86.sh
@@ -8,7 +8,7 @@ pushd "${OVERLAY}" > /dev/null
 for pkg in *-*/* ; do
 	if [[ -d ${CVS_DIR}/${pkg} ]] ; then
 		echo ">>> ${pkg}"
-		diff -ru -x CVS -x ChangeLog -x Manifest "${CVS_DIR}"/${pkg} ${pkg}
+		diff -ru -x CVS -x ChangeLog -x Manifest -I "^# \$Header:" "${CVS_DIR}"/${pkg} ${pkg}
 	fi
 done
 
